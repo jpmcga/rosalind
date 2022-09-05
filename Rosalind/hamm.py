7082c7a023115f4c3a24a63789136d26e7455a6a
james-atlas <jamesm@atlasxomics.com>
2022-09-05 15:36:35 -0400

    deleting old files

--- Rosalind/hamm.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def get_hamming(s1, s2):
-	return sum([1 for pos in range(len(s1)) if s1[pos] != s2[pos]])
-
-if __name__ == '__main__':
-	input = open(sys.argv[1]).read().split('\n')
-	print(get_hamming(input[0], input[1]))
