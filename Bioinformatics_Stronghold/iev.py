6a7c0e4765add309b6b44b14bea7e261b48e0269
james-atlas <jamesm@atlasxomics.com>
2022-09-13 17:23:38 -0400

    Move exception to function for iev.py

--- Bioinformatics_Stronghold/iev.py
+++ Bioinformatics_Stronghold/iev.py
@@ -9,6 +9,10 @@ def calc_dominant_offspring(mating_pairs, offspring=2):
     return expected number of offspring displaying the dominant phenotype.
     ''' 
 
+    if len(mating_pairs) != 6:
+        raise ValueError("Enter mating pairs as list in order 'AA-AA, AA-Aa, \
+        AA-aa, Aa-Aa, Aa-aa, aa-aa")
+
     mp_dom_prob = dict(zip(sorted(mdt), mating_pairs))
     ratio_dominant = sum([mdt[i]*mp_dom_prob[i] for i in mp_dom_prob])
 
@@ -18,13 +22,7 @@ def calc_dominant_offspring(mating_pairs, offspring=2):
 if __name__ == '__main__':
 
     mating_pairs = input('Input mating pair values: ').split()
-    
-    if len(mating_pairs) != 6:
-        raise ValueError("Enter mating pairs as list in order 'AA-AA, AA-Aa, \
-        AA-aa, Aa-Aa, Aa-aa, aa-aa")
-    
     mating_pairs = [int(i) for i in mating_pairs]
     
     print(calc_dominant_offspring(mating_pairs))
-
 
