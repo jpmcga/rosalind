a13b5443dc3a5fa0ec19cf0648cd302c04bb3c81
james-atlas <jamesm@atlasxomics.com>
2022-09-21 13:51:57 -0400

    Add solution to Independent alleles

---
+++ Bioinformatics_Stronghold/lia.py
@@ -0,0 +1,34 @@
+import math
+
+
+def binomial(trials: int, num_success: int, p_success: int):
+    '''
+    Return the binomial probability P(X=1) given number of trials, 
+    number of successes (X), and the probabilty of sucess.
+    '''
+
+    combs = math.comb(trials, num_success)
+
+    return combs * p_success**num_success * (1-p_success)**(trials-num_success)
+
+
+def cumulative_binomial_gte(trials, num_success, p_success):
+    '''
+    Return the cumulative binomial probability that AT LEAST X success will
+    occur P(X>=1) given number of trials, number of successes (X), and the
+    probabilty of sucess.
+    '''
+
+    cumulative_prb = 0
+    for i in range(num_success):
+        cumulative_prb += binomial(trials, i, p_success)
+
+    return 1 - cumulative_prb
+
+
+if __name__ == '__main__':
+    import sys
+
+    offspring = 2**int(input("Enter generation number: "))
+    double_hetero = int(input("Enter double hetero number: "))
+    print(cumulative_binomial_gte(offspring, double_hetero, p_success=0.25)) 
