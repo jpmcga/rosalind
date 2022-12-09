fb60a77720c514189814ee94a57dcb2ec3a2b9aa
James McGann <jamesm@ATX-MBP-RQ05P.local>
2022-12-09 18:52:12 -0500

    Revised solution to perm.py

---
+++ stronghold/perm.py
@@ -0,0 +1,18 @@
+# Solution for Enumerating Gene Orders by Rosalind.com
+# See https://rosalind.info/problems/perm/
+# Solved June 2020, revised December 2022
+
+from itertools import permutations
+
+def permutate_int(n):
+
+	return list(permutations(list(range(1, n+1)))) # shift for 1-indexing
+
+if __name__ == '__main__':
+
+	permutations = permutate_int(int(input()))
+	res = f'{len(permutations)}\n'
+	for perm in permutations:
+		res += ' '.join([str(i) for i in perm]) + '\n'
+	with open('results/result_perm.txt', 'w') as out:
+		out.write(res[:-1])
