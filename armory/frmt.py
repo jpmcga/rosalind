bc6b7db1f21bfc115d4ad66d4c78e885413a8a7f
James McGann <jamesm@ATX-MBP-RQ05P.local>
2023-01-04 13:24:27 -0500

    Revise solution for frmt.py

--- armory/frmt.py
+++ armory/frmt.py
@@ -1,36 +1,26 @@
-#!/usr/local/bin/python3.8
+#!/Users/jamesm/opt/anaconda3/bin/python3
+
+# Solution for Data Formats by Rosalind.com
+# See https://rosalind.info/problems/frmt/
+# Solved Aug 2020, revised Jan 2023
+
 from Bio import Entrez, SeqIO
-import sys
 
+def shortest_fasta(ids, email = ''):
 
-def shortest_fasta(ids, email = ""):
+    Entrez.email = email
+    handle = Entrez.efetch(db='nucleotide', id=ids, rettype='fasta')
+    shortest = min(list(SeqIO.parse(handle, 'fasta')) , key=len).format('fasta')
+    handle.close()
 
-	Entrez.email = email
-	handle = Entrez.efetch(db="nucleotide", id=ids, rettype="fasta")
-	shortest = min(list(SeqIO.parse(handle, "fasta")) , key=len).format('fasta')
-	handle.close()
-
-	return shortest
-
-
-def get_ids(filename):
-
-	file = open(filename)
-	ids = file.read().strip()
-	file.close()
-	
-	return ids
-
-
-def save_fasta(fasta, output_file):
-	file = open(output_file, "w")
-	file.write(fasta)
-	file.close()
-
-	return
+    return shortest
 
 
 if __name__ == '__main__':
+    import sys
 
-	filename = sys.argv[1]
-	save_fasta(shortest_fasta(get_ids(filename)), f"{filename[:-4]}_result.txt")
+    filename = sys.argv[1]
+    outname = 'results/result_frmt.txt'
+    with open(filename) as input, open(outname, 'w') as out:
+        fasta = shortest_fasta(input.read().strip())
+        out.write(fasta)
