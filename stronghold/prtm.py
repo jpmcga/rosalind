5dcdd92dbf5aebad11b3d34cf1933700c4f7bf7c
jpmcga <jpaulmcgann@gmail.com>
2023-03-02 23:16:25 -0500

    Fix tabs in prtm.

--- stronghold/prtm.py
+++ stronghold/prtm.py
@@ -5,12 +5,11 @@
 from utils import aa_mass_table
 
 def calc_peptide_mass(peptide):
-	
-	return sum(aa_mass_table[aa] for aa in peptide)
+
+    return sum(aa_mass_table[aa] for aa in peptide)
 
 if __name__ == "__main__":
     import sys
     
-	with open(sys.argv[1]) as f:
-		print("%.3f" % calc_peptide_mass(f.read().rstrip()))
-
+    with open(sys.argv[1]) as f:
+        print("%.3f" % calc_peptide_mass(f.read().rstrip()))
